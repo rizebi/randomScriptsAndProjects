4f8337cf780b8b92383bac54f5ece20dd60fec53
Eusebiu Rizescu <rizescueusebiu@gmail.com>
2021-06-05 18:31:12 +0300

    Continue lambdaPythonGeneratePreviewFiles

--- lambdaPythonGeneratePreviewFiles/README.md
+++ lambdaPythonGeneratePreviewFiles/README.md
@@ -2,11 +2,11 @@
 docker build -t lambda-generate-preview-image .
 
 ### How to test the image locally
-#####Start container for above image, and mount a volume
+##### Start container for above image, and mount a volume
 docker run -p 9000:8080 -v /Users/eusebiu.rizescu/Data/Git/randomScriptsAndProjects/lambdaPythonGeneratePreviewFiles:/mnt/data lambda-generate-preview-image
 
-#####Invoke function
-curl -XPOST "http://localhost:9000/2015-03-31/functions/function/invocations" -d '{"src_bucket": "", "src_path": "/mnt/data/pig.jpg", "dest_bucket": "", "dest_path": "/mnt/data/output.jpg", "width": "500", "height": "1000", "format": "png"}'
+##### Invoke function
+curl -XPOST "http://localhost:9000/2015-03-31/functions/function/invocations" -d '{"src_bucket": "", "src_path": "/mnt/data/pig.jpg", "dest_bucket": "", "dest_path": "/mnt/data/output.jpg", "width": "500", "height": "1000", "format": "png", "ok_sns": "arn::blabla", "error_sns": "arn::blabla"}'
 
 ### How to create the Lambda Function
 1) Upload previously created image to ECR (container registry for AWS)
@@ -15,8 +15,31 @@ curl -XPOST "http://localhost:9000/2015-03-31/functions/function/invocations" -d
 
 3) Ensure that lambda has permissions to Read on the source bucket and Read-Write on the destination bucket
 
+4) Increase timeout to 5 minutes (default is 3 seconds which is too little)
+
 ### How to use the function
 
-Invoke function like this:
+- Invoke function like this from command line:
 
-aws lambda invoke --region <function_region> --function-name <function_name> --payload '{"src_bucket": "source-bucket-name", "src_path": "/path/to/pig.jpg", "dest_bucket": "dest-bucket-name", "dest_path": "/path/to/pig-preview.jpg", "width": "500", "height": "1000", "format": "jpg"}' invoke_function_response
+aws lambda invoke --region us-west-2 --function-name lambda-generate-preview-image --cli-binary-format raw-in-base64-out --payload '{"src_bucket": "lambda-generate-preview-image-source", "src_path": "source_folder/pig.jpg", "dest_bucket": "lambda-generate-preview-image-destination", "dest_path": "destination_folder/pig-preview.jpg", "width": "500", "height": "1000", "format": "jpg", "ok_sns": "arn:aws:sns:us-west-2:020275857710:lambda-generate-preview-image-ok-topic", "error_sns": "arn:aws:sns:us-west-2:020275857710:lambda-generate-preview-image-error-topic"}' invoke_function_response
+
+- For test we can go to Lambda console, and launch an test event
+
+- NOTE! That the src_path and dest_path MUST not have a forward slash. They should start without it.
+
+### How to update the code
+
+-  Update code
+
+-  Build image
+
+docker build -t lambda-generate-preview-image .
+-  Push image to ECR
+
+docker tag lambda-generate-preview-image:latest 020275857710.dkr.ecr.us-west-2.amazonaws.com/lambda-generate-preview-image:latest
+
+docker push 020275857710.dkr.ecr.us-west-2.amazonaws.com/lambda-generate-preview-image:latest
+
+-  Update the Lambda to use the new image
+
+-  Delete older image from ECR as no needed
--- lambdaPythonGeneratePreviewFiles/lambda_function.py
+++ lambdaPythonGeneratePreviewFiles/lambda_function.py
@@ -1,4 +1,5 @@
 import boto3
+import traceback
 from shutil import copyfile
 from PIL import Image, ImageOps
 from preview_generator.manager import PreviewManager
@@ -6,6 +7,7 @@ from preview_generator.manager import PreviewManager
 def lambda_handler(event, context):
 
   ### Inputs
+  print("Event: " + str(event))
   src_bucket = event["src_bucket"]
   src_path = event["src_path"]
   dest_bucket = event["dest_bucket"]
@@ -13,45 +15,82 @@ def lambda_handler(event, context):
   wanted_width = int(event["width"])
   wanted_height = int(event["height"])
   output_format = event["format"]
+  ok_sns = event["ok_sns"]
+  error_sns = event["error_sns"]
 
-  ### Copy the file from S3:
-  if src_bucket != "":
-    client = boto3.client('s3')
-    source_file = "/tmp/" + src_path.split("/")[-1]
-    s3.download_file(src_bucket, src_path, source_file)
-  else:
-    # If the src_bucket == 0 we will not copy from S3, and use local path.
-    # This is for local testing
-    source_file = src_path
+  try:
+    ### Copy the file from S3:
+    print("Copy file from source S3")
+    if src_bucket != "":
+      client = boto3.client('s3')
+      source_file = "/tmp/" + src_path.split("/")[-1]
+      client.download_file(src_bucket, src_path, source_file)
+    else:
+      # If the src_bucket == 0 we will not copy from S3, and use local path.
+      # This is for local testing
+      source_file = src_path
+    print("Successfully copied file from source S3")
 
-  ### Generate preview
-  manager = PreviewManager("./cache", create_folder= True)
-  preview_image = manager.get_jpeg_preview(source_file, width=wanted_width, height=wanted_height)
+    ### Generate preview
+    print("Generating preview")
+    manager = PreviewManager("/tmp/cache", create_folder= True)
+    preview_image = manager.get_jpeg_preview(source_file, width=wanted_width, height=wanted_height)
+    print("Successfully generated preview")
 
-  ### Make sure the image has the expected size and format
-  # Open the preview image
-  img = Image.open(preview_image)
+    ### Make sure the image has the expected size and format
+    print("Resize and change the format")
+    # Open the preview image
+    img = Image.open(preview_image)
 
-  # Calculate the differences between actual sizes and wanted sizes
-  delta_w = wanted_width - img.width
-  delta_h = wanted_height - img.height
+    # Calculate the differences between actual sizes and wanted sizes
+    delta_w = wanted_width - img.width
+    delta_h = wanted_height - img.height
 
-  # Then actual sizes is odd (not even), the we need to add one more pixel
-  precision_pixel_w = wanted_width - img.width - int(delta_w/2) - int(delta_w-(delta_w/2))
-  precision_pixel_h = wanted_height - img.height - int(delta_h/2) - int(delta_h-(delta_h/2))
+    # Then actual sizes is odd (not even), the we need to add one more pixel
+    precision_pixel_w = wanted_width - img.width - int(delta_w/2) - int(delta_w-(delta_w/2))
+    precision_pixel_h = wanted_height - img.height - int(delta_h/2) - int(delta_h-(delta_h/2))
 
-  # Resize the image
-  ltrb_border=(int(delta_w/2), int(delta_h/2), int(delta_w-(delta_w/2)) + precision_pixel_w, int(delta_h-(delta_h/2)) + precision_pixel_h)
-  img_with_border = ImageOps.expand(img, border=ltrb_border, fill='white')
-  output_file = "/tmp/output_preview." + output_format
-  img_with_border.save(output_file)
+    # Resize the image
+    ltrb_border=(int(delta_w/2), int(delta_h/2), int(delta_w-(delta_w/2)) + precision_pixel_w, int(delta_h-(delta_h/2)) + precision_pixel_h)
+    img_with_border = ImageOps.expand(img, border=ltrb_border, fill='white')
+    output_file = "/tmp/output_preview." + output_format
+    img_with_border.save(output_file)
+    print("Successfully resized and formatted")
 
-  ### Copy to S3
-  if dest_bucket != "":
-    client = boto3.client('s3')
-    s3.upload_file(output_file, dest_bucket, dest_file)
-  else:
-    # If the dest_bucket == 0 we will not copy from S3, but will copy on local path
-    copyfile(output_file, dest_path)
+    ### Copy to S3
+    print("Copy preview to destination bucket")
+    if dest_bucket != "":
+      client = boto3.client('s3')
+      client.upload_file(output_file, dest_bucket, dest_path)
+    else:
+      # If the dest_bucket == 0 we will not copy from S3, but will copy on local path
+      copyfile(output_file, dest_path)
+    print("Successfully copied preview to destination bucket")
 
-  return "OK"
+    ### Send ok message to ok SNS
+    print("Sending OK to SNS")
+    if src_bucket != "":
+      # If the src_bucket is not empty, then the function is running locally for dev purposes, so no need to send SNS
+      client = boto3.client('sns')
+      response = client.publish(
+          TopicArn=ok_sns,
+          Message="Preview generated successfully for: " + src_bucket + src_path
+      )
+    print("Successfully send OK to SNS")
+    return "OK"
+
+  except Exception as e:
+    print("Error: {}".format(e))
+    tracebackError = traceback.format_exc()
+    print(tracebackError)
+    ### Send error message to error SNS
+    print("Sending ERROR to SNS")
+    if src_bucket != "":
+      # If the src_bucket is not empty, then the function is running locally for dev purposes, so no need to send SNS
+      client = boto3.client('sns')
+      response = client.publish(
+          TopicArn=error_sns,
+          Message="Preview FAILED to generate for: " + src_bucket + src_path
+      )
+    print("Successfully send ERROR to SNS")
+    return "ERROR"
