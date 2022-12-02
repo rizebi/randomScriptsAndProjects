c6a037c8455b742ac9f535d1524c6aaeb3dc56a8
Eusebiu Rizescu <rizescueusebiu@gmail.com>
2022-12-03 00:00:23 +0200

    [piAssistant] First working version. Template ready, and working. Commands can be added in config.json

--- piAssistant/piAssistant.py
+++ piAssistant/piAssistant.py
@@ -1,146 +1,138 @@
 import os
 import json
-import signal
+import time
+import logging # for logging
 import platform # to guess the host we are on
-import urllib.request as request
-
+import datetime # for logging
+import subprocess # for executing commands
 from pathlib import Path # to read the password file
-from datetime import datetime
-from urllib.error import HTTPError
-from http.client import HTTPResponse
-from typing import Dict, List, Union
+import urllib.request as request # to send messages
+logging.getLogger('telethon').setLevel(logging.ERROR) # to avoid telethon log spam
+from telethon import TelegramClient, events # to receive messages
 
-# Read token
+# Read credentials
 if "macOS" in platform.platform():
-  tokenFilePath = "/Users/eusebiurizescu/.telegram"
+  tokenFolderPath = "/Users/eusebiurizescu/.telegram"
 else:
-  tokenFilePath = "/root/Tools/Config"
-TOKEN = (Path(tokenFilePath) / "telegramToken").read_text().replace("\\n", "").replace("\n", "")
+  tokenFolderPath = "/root/Tools/Config"
 
-signal.signal(signal.SIGINT, signal.SIG_DFL)
+currentDir = os.getcwd()
+botToken = (Path(tokenFolderPath) / "telegramBotToken").read_text().replace("\\n", "").replace("\n", "")
+apiId = (Path(tokenFolderPath) / "telegramApiId").read_text().replace("\\n", "").replace("\n", "")
+apiHash = (Path(tokenFolderPath) / "telegramApiHash").read_text().replace("\\n", "").replace("\n", "")
+channelIdList = [-442381868]
+client = TelegramClient('bot', apiId, apiHash)
 
+configFile = open("config.json")
+config = json.load(configFile)
 
-class TelegramEcho:
-  def __init__(self, TG_KEY: str):
-    self.TG_URL = "https://api.telegram.org/bot{key}/{method}"
-    self.TG_KEY = TG_KEY
+# Logging function
+def getLogger():
+  # Create logs folder if not exists
+  if not os.path.isdir(os.path.join(currentDir, "logs")):
+    try:
+      os.mkdir(os.path.join(currentDir, "logs"))
+    except OSError:
+      print("Creation of the logs directory failed")
+    else:
+      print("Successfully created the logs directory")
 
-    self.__last = None
-    self.__last_time = None
-    pass
+  now = datetime.datetime.now()
+  log_name = "" + str(now.year) + "." + '{:02d}'.format(now.month) + "." + '{:02d}'.format(now.day) + "-piAssistant.log"
+  log_name = os.path.join(currentDir, "logs", log_name)
+  logging.basicConfig(format='%(asctime)s  %(message)s', level=logging.NOTSET,
+                      handlers=[
+                      logging.FileHandler(log_name),
+                      logging.StreamHandler()
+                      ])
+  log = logging.getLogger()
+  return log
 
-  def run(self):
-    """
-    method to handle the incoming message and the send echo message to the user
-    """
-    while True:
-      try:
-        # getting the incoming data
-        incoming = self.__handle_incoming()
+def executeCommand(log, command):
+  error = ""
+  output = ""
+  log.info("Executing: " + command)
+  try:
+    output = subprocess.check_output(command, shell=True, stderr=subprocess.STDOUT)
+    output = output.decode('utf-8')
+  except Exception as e:
+    error = "ERROR: " + str(e.returncode) + "  " + str(e) + "\n"
+    output = repr(e)  # to get the output even when error
+  #log.info("Output: " + output)
+  return output, error
 
-        # checking if incoming message_id is same as of last, then skip
-        if self.__last == incoming["message"]["message_id"]:
-          continue
-        else:
-          self.__last = incoming["message"]["message_id"]
+def sendMessage(log, message):
+  log.info("Sending message: " + message)
+  # Create the payload
+  payload = {"chat_id": channelIdList[0], "text": message }
+  # Create the request
+  requestObj = request.Request( "https://api.telegram.org/bot" + botToken + "/sendMessage",
+    data=json.dumps(payload).encode('utf8'),
+    headers={"Content-Type": "application/json"})
+  # Send the message
+  sendMessage = request.urlopen(requestObj)
+  result = json.loads(sendMessage.read().decode())["result"]
+  log.info(result)
 
-        # adding more validation to prevent messaging the last message whenever the polling starts
-        if not self.__last_time:
-          self.__last_time = incoming["message"]["date"]
-          continue
-        elif self.__last_time < incoming["message"]["date"]:
-          self.__last_time = incoming["message"]["date"]
-        else:
-          continue
-
-        # finally printing the incoming message
-        self.__print_incoming(incoming)
-
-        # now sending the echo message
-        outgoing = self.__handle_outgoing(
-          incoming["message"]["chat"]["id"],
-          incoming["message"]["text"])
-
-        # finally printing the outgoing message
-        self.__print_outgoing(outgoing)
-
-        pass
-      except (HTTPError, IndexError):
-        continue
-      pass
-    pass
-
-  def __handle_incoming(self) -> Dict[str, Union[int, str]]:
-    """
-    method fetch the recent messages
-    """
-
-    # getting all messages
-    getUpdates = request.urlopen(
-      self.TG_URL.format(key=self.TG_KEY, method="getUpdates"))
-
-    # parsing results
-    results: List[Dict[str, Union[int, str]]] = json.loads(
-      getUpdates.read().decode())["result"]
-
-    # getting the last error
-    return results[-1]
-
-  def __print_incoming(self, incoming: Dict[str, Union[int, str]]):
-    """
-    method to print the incoming message on console
-    """
-    print("[<<<] Message Recieved on %s" % datetime.fromtimestamp(
-      incoming["message"]["date"]).strftime("%Y-%m-%d %H:%M:%S"))
-    print("\tText: %s" % incoming["message"]["text"])
-    print("\tFrom: %s" %
-        incoming["message"]["from"].get("first_name", "") + " " +
-        incoming["message"]["from"].get("last_name", ""))
-    print("\tMessage ID: %d" % incoming["message"]["message_id"])
-    print("-" * os.get_terminal_size().columns)
-    pass
-
-  def __handle_outgoing(self, chat_id: int,
-              message_txt: str) -> Dict[str, Union[int, str]]:
-    """
-    method to send the echo message to the same chat
-    """
-
-    # making the post data
-    _data: Dict[str, Union[int, str]] = {
-      "chat_id":
-      chat_id,
-      "text":
-      "You sent me \"{MESSAGE_TEXT}\"".format(MESSAGE_TEXT=message_txt)
-    }
-
-    # creating the request
-    _request: request.Request = request.Request(
-      self.TG_URL.format(key=self.TG_KEY, method="sendMessage"),
-      data=json.dumps(_data).encode('utf8'),
-      headers={"Content-Type": "application/json"})
-
-    # sending HTTP request, for sending message to the user
-    sendMessage: HTTPResponse = request.urlopen(_request)
-    result: Dict[str, Union[int, str]] = json.loads(
-      sendMessage.read().decode())["result"]
-    return result
-
-  def __print_outgoing(self, outgoing):
-    """
-    method to print outgoing data on the console
-    """
-    print("[>>>] Message Send on %s" % datetime.fromtimestamp(
-      outgoing["date"]).strftime("%Y-%m-%d %H:%M:%S"))
-    print("\tText: %s" % outgoing["text"])
-    print("\tFrom: %s" % outgoing["from"]["first_name"])
-    print("\tMessage ID: %d" % outgoing["message_id"])
-    print("-" * os.get_terminal_size().columns)
-    pass
-
+def runLocal(log, instructions, messageList):
   pass
 
+def runRemote(log, instructions, host, messageList):
+  # Get password
+  if "macOS" in platform.platform():
+    sshPasswordText = Path("/Users/eusebiurizescu/.telegram/localnetflixPass").read_text().replace("\\n", "").replace("\n", "")
+  else:
+    sshPasswordText = Path(host["pass"]).read_text().replace("\\n", "").replace("\n", "")
+
+  # Replace arguments in instructions command
+  instructionsCommand = instructions["command"]
+  for i in range(10):
+    try:
+      instructionsCommand = instructionsCommand.replace("$" + str(i), "\"" + messageList[i] + "\"")
+    except:
+      pass
+
+  # Execute the command
+  command = "sshpass -p '" + sshPasswordText + "' ssh -p " + host["port"] + " "+ host["user"] + "@" + host["ip"] + " " + instructionsCommand
+  output, error = executeCommand(log, command)
+  log.info("Output = " + str(output))
+  log.info("Error = " + str(error))
+
+  return str(error).replace(sshPasswordText, "SECRET")
+
+def processMessage(log, receivedMessage):
+  messageList = receivedMessage.split(" ")
+  if messageList[0] in config["commands"].keys():
+    instructions = config["commands"][messageList[0]]
+    isLocal = config["servers"][instructions["host"]]["local"]
+    if isLocal == "yes":
+      error = runLocal(log, instructions, messageList)
+    else:
+      host = config["servers"][instructions["host"]]
+      error = runRemote(log, instructions, host, messageList)
+    if error == "":
+      sendMessage(log, "Done")
+    else:
+      sendMessage(log, "Error: " + str(error))
+  else:
+    sendMessage(log, "Command not recognized")
+
+@client.on(events.NewMessage(channelIdList))
+async def main(event):
+  # Update logger handler
+  log = getLogger()
+  receivedMessage = event.raw_text
+  sender = str(event.from_id).split("=")[1].replace(")", "")
+  if(sender not in botToken):
+    # We do not want to process our messages
+    log.info("Received new message: " + receivedMessage)
+    processMessage(log, receivedMessage)
 
 if __name__ == "__main__":
-  tg = TelegramEcho(TOKEN)
-  tg.run()
+  # Create logger handler
+  log = getLogger()
+
+  client.start()
+  log.info("Client started")
+
+  client.run_until_disconnected()
